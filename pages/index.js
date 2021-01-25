42c0eda89a6e3acfc509e4d28a19f5a9f42e2c33
Elbarae <elbarae1921@gmail.com>
2021-01-25 20:02:24 +0100

    Added NextJS

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Elbarae</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            Mox Adventu...
+        </div>
+    )
+}
