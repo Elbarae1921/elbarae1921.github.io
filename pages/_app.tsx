15c91b86fa215a2a1dc7674e4c9d6ee571c911f8
Elbarae <elbarae1921@gmail.com>
2021-01-29 00:18:31 +0100

    Added cool background

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import './index.scss';
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <Component {...pageProps} />
+    )
+}
+
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import React, { useEffect, useState } from 'react';
+// import ParticlesBg from "particles-bg";
+
 
 export default function Home() {
+
+    const [ParticlesBg, setParticlesBg] = useState(null);
+    useEffect(() => {
+        const mod = dynamic(() => import('particles-bg'));
+        setParticlesBg(mod);
+    }, []);
+
     return (
-        <div>
+        <div id="app">
             <Head>
                 <title>Elbarae</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            Mox Adventu...
+            <div id="bg">
+                { ParticlesBg && <ParticlesBg type="lines" /> }
+            </div>
+            <div className="main">
+                <h1 className="text" data-text="Mox Adventu...">Mox Adventu...</h1>
+            </div>
         </div>
     )
 }
